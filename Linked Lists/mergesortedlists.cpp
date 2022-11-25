524e6737b1b584141fb5e18413603a61a8abdc37
Pranav Pore <pranav@pranavpore.com>
2022-11-25 21:59:09 +0530

    merge sorted lists

---
+++ Linked Lists/mergesortedlists.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int count = 0;
+        
+        for(vector<ListNode*> it : lists){
+            if(it == NULL)count++;
+        }
+
+        if(count == lists.size()) return NULL;
+        
+        vector<int> v;
+
+        for(vector<ListNode*> it : lists){
+            while(it!=NULL){
+                v.push_back(it->val);
+                it = it->next;
+            }
+        }
+
+        sort(v.begin(), v.end());
+        
+        ListNode* a = new ListNode(v[0]);
+        ListNode * b = a;
+        
+        for(int i = 0; i < v.size() - 1; i++){
+            b->next = new ListNode(v[i + 1]);
+            b = b->next;
+        }
+        
+        return a;
+    }
+};
