81c28b1c61c442e48db47885de67f0fa0e345fdb
Pranav Pore <pranav@pranavpore.com>
2022-12-28 18:10:15 +0530

    added firstandlastposition.cpp

---
+++ Searching/firstandlastposition.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <utility>
+
+std::pair<int, int> firstAndLast(int*, int, int);
+
+int main(){
+    int arr[] = {1, 2, 3, 3, 5, 8, 8, 8, 8, 8, 10};
+    std::pair<int, int> result = firstAndLast(arr, sizeof(arr) / sizeof(arr[0]), 70);
+    std::cout<<"Left most occurence is: "<<result.first<<std::endl;
+    std::cout<<"Right most occurence is: "<<result.second<<std::endl; 
+    return 0;
+}
+
+std::pair<int, int> firstAndLast(int* arr, int n, int k){
+    int first = -1;
+    int last = -1;
+
+    int left = 0;
+    int right = n - 1;
+    int mid = left + (right - left) / 2;
+
+    while(left <= right){
+        if(arr[mid] == k){
+            first = mid;
+            right = mid - 1;
+        }
+
+        else if(arr[mid] < k)
+            left = mid + 1;
+        
+        else
+            right = mid - 1;
+
+        mid = left + (right - left) / 2; 
+    }
+
+    left = 0;
+    right = n - 1;
+    mid = left + (right - left) / 2;
+
+    while(left <= right){
+        if(arr[mid] == k){
+            last = mid;
+            left = mid + 1;
+        }
+
+        else if(arr[mid] < k)
+            left = mid + 1;
+        
+        else
+            right = mid - 1;
+
+        mid = left + (right - left) / 2;  
+    }
+
+    return std::pair<int, int>(first, last);
+
+}
